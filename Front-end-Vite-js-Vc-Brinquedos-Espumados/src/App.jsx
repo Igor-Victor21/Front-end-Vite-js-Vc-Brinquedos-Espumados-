74a43aa1ceb7dee44a3e9e2c5245c2a871f5c95d
Vinicius <vina.santos06@gmail.com>
2025-07-08 16:11:33 -0300

    criação do vite

---
+++ Front-end-Vite-js-Vc-Brinquedos-Espumados/src/App.jsx
@@ -0,0 +1,50 @@
+import './App.module.css'
+
+export default function App() {
+
+  return (
+    <>
+      {/* <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+      <section>
+        <button>
+          <image/>
+        </button>
+        <h1>Bom dia Michely!</h1>
+        <p>Tenha momentos divertidos e únicos com seus filhos com a VC brinquedos Espumados</p>
+        <input placeholder='Procurar'></input>
+      </section>
+
+      <h3>Nova Coleção</h3>
+
+      <section>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+      </section>
+
+      <section>
+        {/* cards */}
+      </section>
+    </>
+  )
+}
