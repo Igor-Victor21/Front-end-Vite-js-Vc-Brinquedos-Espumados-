48a219d9f3f363a3bb670346d974514b50479fb1
Vinicius <vina.santos06@gmail.com>
2025-07-14 15:59:27 -0300

    Adição das pastas e api do Backend

--- Front-end-Vite-js-Vc-Brinquedos-Espumados/src/main.jsx
+++ Front-end-Vite-js-Vc-Brinquedos-Espumados/src/main.jsx
@@ -1,18 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider} from 'react-router'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  }
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
