4cdf2bd722fb58bdf169aa892b02c28b893fc4bb
Vinicius <vina.santos06@gmail.com>
2025-07-18 16:10:42 -0300

    tela de cadastro e delete de arquivos não usados

--- Front-end-Vite-js-Vc-Brinquedos-Espumados/src/Login.jsx
+++ Front-end-Vite-js-Vc-Brinquedos-Espumados/src/Login.jsx
@@ -1,9 +1,90 @@
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { api } from '../api/api'
+
 import style from './Login.module.css'
 
+import Vinha from './assets/image/vinha.png'
+import Image from './assets/image/user-test-sem-figma.png'
+
 function Login() {
+  // const navigate = useNavigate()
+
+  // const [email, setEmail] = useState('')
+  // const [password, setPassword] = useState('')
+  // const [user, setUser] = useState(null)
+  // const [message, setMessage] = useState('')
+
+  // useEffect(() => {
+  //   const storedUser = localStorage.getItem('user')
+  //   if (storedUser) {
+  //     const parsedUser = JSON.parse(storedUser)
+  //     setUser(parsedUser)
+  //     if(parsedUser.email === 'igor.victorcontato@gmail.com'){
+  //       navigate('/UserAdmin')
+  //     }else{
+  //       navigate('/User')
+  //     }
+  //   }
+  // }, [navigate])
+
+  // const handleLogin = async (e) => {
+  //   e.preventDefault();
+
+  //   try {
+  //     const response = await api.get('/users');
+  //     const users = response.data;
+  //     const foundUser = users.find(
+  //       (u) => u.email === email && u.password === password
+  //     );
+  //     if (!foundUser) {
+  //       setMessage("Email ou senha incorretos");
+  //       setTimeout(() => setMessage(""), 3000);
+  //       return;
+  //     }
+  //     localStorage.setItem('user', JSON.stringify(foundUser));
+  //     setUser(foundUser);
+
+  //     //verifica se é admin
+  //     if (foundUser.email === 'igor.victorcontato@gmail.com') {
+  //       navigate('/UserAdmin');
+  //     } else {
+  //       navigate('/User');
+
+  //     }
+
+  //   } catch (error) {
+  //     setMessage('Erro ao tentar logar: ' + (error.message || 'Erro desconhecido'));
+  //     setTimeout(() => setMessage(""), 3000);
+  //   }
+  // };
+
+  // return (
+  //   <div style={{ padding: '2rem' }}>
+  //     <form onSubmit={handleLogin}>
+  //       <h2>Login</h2>
+  //       <input
+  //         type="email"
+  //         placeholder="Email"
+  //         value={email}
+  //         onChange={(e) => setEmail(e.target.value)}
+  //         required
+  //       />
+  //       <br />
+  //       <input
+  //         type="password"
+  //         placeholder="Senha"
+  //         value={password}
+  //         onChange={(e) => setPassword(e.target.value)}
+  //         required
+  //       />
+  //       <br />
+  //       <button type="submit">Entrar</button>
+  //       <p>{message}</p>
+  //     </form>
+  //   </div>
+  // )
+
   const navigate = useNavigate()
 
   const [email, setEmail] = useState('')
@@ -56,30 +137,65 @@ function Login() {
   };
 
   return (
-    <div style={{ padding: '2rem' }}>
-      <form onSubmit={handleLogin}>
-        <h2>Login</h2>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <br />
-        <button type="submit">Entrar</button>
-        <p>{message}</p>
-      </form>
-    </div>
+    <>
+      <section className={style.container}>
+        <div className={style.wrapCadastro}>
+          <img className={style.upperVine} src={Vinha} alt='vinha superior'/>
+          <form className={style.cadastro} onSubmit={handleLogin}>
+            <div>
+              <img className={style.leftVine} src={Vinha} alt='vinha esquerda'/>
+              <img className={style.rightVine} src={Vinha} alt='vinha direita'/>
+            </div>
+            <h1 className={style.h1}>Bem vindo(a) de volta!!</h1>
+            <p className={style.p}>Digite suas credenciais para acessar sua conta</p>
+            <p className={style.pInput}>Nome</p>
+            <input 
+            className={style.input} 
+            type="text" 
+            placeholder='Digite seu nome'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required/>
+            <p className={style.pInput}>E-mail</p>
+            <input 
+            className={style.input} 
+            type="email" 
+            placeholder='Digite seu email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required/>
+            <p className={style.pInput}>Senha</p>
+            <input 
+            className={style.input} 
+            type="password" 
+            placeholder='Digite sua senha'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required/>
+            <p className={style.pInput}>Confirmar Senha</p>
+            <input 
+            className={style.input} 
+            type="password" 
+            placeholder='Confirme sua senha'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required/>
+            <button className={style.button} type='submit'>Registrar-se</button>
+            <p>{message}</p>
+            <p className={style.ou}>Ou</p>
+            <p className={style.pLink}>Já tem uma conta? <Link to="/" className={style.link}>Entre aqui</Link></p>
+          </form>
+        </div>
+        <div className={style.wrapImage}>
+          <img 
+          className={style.img}
+          src={Image}
+          alt='teste'/>
+        </div>
+      </section>
+    </>
   )
+
 }
 
 export default Login
